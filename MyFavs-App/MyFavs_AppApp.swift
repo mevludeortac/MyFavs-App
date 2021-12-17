bde3e5e950bf5e0065fc1df4cfbe21b3a9349cbb
goldbilgisayar <ortacmevlude@gmail.com>
2021-12-18 01:42:22 +0300

    Initial Commit

---
+++ MyFavs-App/MyFavs_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFavs_AppApp.swift
+//  MyFavs-App
+//
+//  Created by MEWO on 18.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct MyFavs_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
