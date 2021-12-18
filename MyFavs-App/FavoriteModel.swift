15234a8b4c651386a8552affc2f34aae84b3088e
goldbilgisayar <ortacmevlude@gmail.com>
2021-12-18 17:24:26 +0300

    models has been written

---
+++ MyFavs-App/FavoriteModel.swift
@@ -0,0 +1,33 @@
+//
+//  FavoriteModel.swift
+//  MyFavs-App
+//
+//  Created by MEWO on 18.12.2021.
+//
+
+import Foundation
+
+struct FavoriteModel: Identifiable {
+    var id = UUID()
+    var title : String
+    var elements: [FavoriteElements]
+}
+
+struct FavoriteElements: Identifiable {
+    var id = UUID()
+    var name: String
+    var imageName: String
+    var description: String
+}
+//BANDS
+let hedonutopia = FavoriteElements(name: "Hedonutopia", imageName: "hedon", description: "number one")
+let gevende = FavoriteElements(name: "gevende", imageName: "gevende", description: "number two")
+let oscarAndTheWolf = FavoriteElements(name: "Oscar And The Wolf", imageName: "oscar", description: "number three")
+
+let favoriteBands = FavoriteModel(title: "Favorite Bands", elements: [hedonutopia, gevende, oscarAndTheWolf])
+
+let yazgi = FavoriteElements(name: "Yazgi", imageName: "yazgi", description: "number one")
+let yeralti = FavoriteElements(name: "Yeraltı", imageName: "yeralti", description: "number two")
+let kader = FavoriteElements(name: "kader", imageName: "kader", description: "numbre three")
+
+let favoriteMovie = FavoriteModel(title: "Favorie Movies", elements: [yazgi, yeralti, kader])
